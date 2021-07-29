3d89964d9dc0fe25fe147d71f5aaa3a3d4cc4b9a
VivaEcha838 <68964649+VivaEcha838@users.noreply.github.com>
2021-07-29 09:39:17 -0700

    Add files via upload

---
+++ bellCurves.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import plotly.figure_factory as ff
+
+df = pd.read_csv("data.csv")
+
+fig = ff.create_distplot([df["Weight(Pounds)"].tolist()], ["weight"], show_hist= False)
+fig.show()
+
